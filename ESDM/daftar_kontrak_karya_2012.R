f101e12ab53412066939e07ff23098a9c6e7fd4e
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2015-06-26 22:56:43 +0700

    ESDM: daftar_kontrak_karya_2012

---
+++ ESDM/daftar_kontrak_karya_2012.R
@@ -0,0 +1,17 @@
+# Ambil data
+kontrak_karya <- read.csv(
+  "http://data.ukp.go.id/storage/f/2014-04-20T14%3A52%3A57.039Z/processed-daftar-kontrak-karya-2012.csv",
+  header=TRUE,
+  sep=",")
+
+# Ringkasan Data
+summary(kontrak_karya)
+
+# Total wilayah
+sum(crime$luas_wilayah)
+
+# Pemegang kontrak karya dengan luas wilayah terkecil
+subset(kontrak_karya,luas_wilayah==min(kontrak_karya$luas_wilayah))
+
+# Pemegang kontrak karya dengan luas wilayah terbesar
+subset(kontrak_karya,luas_wilayah==max(kontrak_karya$luas_wilayah))
